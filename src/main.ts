929a128f55423f0522a011455174b750a52bb9fc
PedroCode2 <Pedro.hsoares131@gmail.com>
2022-04-23 21:41:01 -0300

    Adicionando tudo que vai ser usado e configurando db

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  app.useGlobalPipes(new ValidationPipe());
+
 
   const config = new DocumentBuilder()
     .setTitle('Animes and Games Swords')
     .setDescription('A Sword api of Games and Animes')
     .setVersion('1.0')
-    .addTag('Swords')
+    .addTag('swords')
+    .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
